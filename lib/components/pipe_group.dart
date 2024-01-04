4c16d0a4f03e8144291a48590bbf33a7463fb7f1
colmanpy <christiancolmanpy@gmail.com>
2024-01-04 20:55:03 -0300

    feat: add random height for pipes and remove when dissapear

--- lib/components/pipe_group.dart
+++ lib/components/pipe_group.dart
@@ -15,9 +15,13 @@ class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
 
     final heightMinusGround = gameRef.size.y - Config.groundHeight;
     final spacing = 100 + _random.nextDouble() * (heightMinusGround / 4);
+    final centerY =
+        spacing + _random.nextDouble() * (heightMinusGround - spacing);
     addAll([
-      Pipe(height: 100, pipePosition: PipePosition.top),
-      Pipe(height: 200, pipePosition: PipePosition.bottom),
+      Pipe(height: centerY - spacing / 2, pipePosition: PipePosition.top),
+      Pipe(
+          height: heightMinusGround - (centerY + spacing / 2),
+          pipePosition: PipePosition.bottom),
     ]);
   }
 
@@ -25,5 +29,10 @@ class PipeGroup extends PositionComponent with HasGameRef<FlappyBirdGame> {
   void update(double dt) {
     super.update(dt);
     position.x -= Config.gameSpeed * dt;
+
+    if (position.x < -10) {
+      removeFromParent();
+      print('Removed');
+    }
   }
 }
